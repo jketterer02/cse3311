69120ffe9dad60acc3de9003ab0699772f1022ec
jketterer02 <jketterer02@gmail.com>
2024-02-06 20:37:40 -0600

    initial GUI code

--- gui_test.java
+++ gui_test.java
@@ -1,10 +1,29 @@
 import javax.swing.*;
+import java.awt.FlowLayout;
+import java.awt.BorderLayout;
 
+public class gui_test extends JFrame {
 
-public class gui_test
-{
-    public static void main(String []args)
+    public gui_test() {
+        //Title
+        super("GUI Test");
+        //Layout Type
+        setLayout(new FlowLayout());
+        //Button
+        JButton button = new JButton("Click me!");
+        //Adds button
+        add(button);
+        //Exits when closed
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //Size
+        setSize(300, 200);
+        //visiblity
+        setVisible(true);
+    }
+
+    public static void main(String[] args)
     {
-        System.out.print("Hello World!");
+        //creates the GUI
+        gui_test myFrame = new gui_test();
     }
 }
